f6eca743f8b6691db12506698b260842e8205250
suri <swigder@gmail.com>
2021-05-06 20:43:47 -0400

    Add HTML page showing data.

--- main.py
+++ main.py
@@ -1,29 +1,93 @@
+import json
+
 import pandas as pd
 import requests
-import matplotlib.pyplot as plt
+import geojson
+
+BASE_URL = 'https://crashviewer.nhtsa.dot.gov'
+CRASH_API = f'{BASE_URL}/CrashAPI/crashes'
+
+
+def query_crash_api(api, params, cache=True):
+    def get_filename():
+        inner_name = None
+        if api == 'GetCrashesByLocation':
+            inner_name = 'GetCrashesByLocation'
+        if api == 'GetCaseDetails':
+            inner_name = f'GetCaseDetails-{params["caseYear"]}-{params["stateCase"]}'
+        return f'cache/{inner_name}.json' if inner_name else None
+
+    filename = get_filename()
+    if cache and filename:
+        try:
+            with open(filename) as infile:
+                return json.load(infile)
+        except IOError:
+            pass
+    params['format'] = 'json'
+    response = requests.get(f'{CRASH_API}/{api}', params=params).json()
+    if cache and filename:
+        with open(filename, 'w') as outfile:
+            json.dump(response, outfile)
+    return response
+
+
+def convert_to_geojson(df):
+    features = []
+    harm_to_symbol = {
+        'Motor Vehicle In-Transport': 'car',
+        'Parked Motor Vehicle': 'car',
+        'Pedalcyclist': 'bike',
+        'Pedestrian': 'ped'
+    }
+    for index, row in df.iterrows():
+        harm = row['Harm'].strip()
+        features.append(geojson.Feature(geometry=geojson.Point((row['LONGITUD'], row['LATITUDE'])), properties={
+            'year': row['CaseYear'],
+            'case_id': row['ST_CASE'],
+            'harm': harm_to_symbol[harm] if harm in harm_to_symbol else 'other',
+        }))
+    return geojson.FeatureCollection(features=features)
+
+
+def get_people(crash_result_set):
+    people = crash_result_set['NPersons'] or []
+    for vehicle in crash_result_set['Vehicles']:
+        people.extend(vehicle['Persons'])
+    return people
 
 
 def get_crashes_list(year_range=(2015, 2019), state_id=24, county_id=33):
-    response = requests.get('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCrashesByLocation',
-                            params={'state': state_id, 'county': county_id, 'fromCaseYear': year_range[0], 'toCaseYear': year_range[1],
-                                    'format': 'json'})
-    df = pd.DataFrame(response.json()['Results'][0])
+    response = query_crash_api(api='GetCrashesByLocation',
+                               params={'state': state_id, 'county': county_id, 'fromCaseYear': year_range[0],
+                                       'toCaseYear': year_range[1]})
+
+    df = pd.DataFrame(response['Results'][0])
     df[["LONGITUD", "LATITUDE"]] = df[["LONGITUD", "LATITUDE"]].apply(pd.to_numeric)
 
     bbox = (-76.9862, -76.7740, 39.0560, 38.9376)
 
-    ndf = df[(bbox[0] < df["LONGITUD"]) & (df["LONGITUD"] < bbox[1]) & (bbox[3] < df["LATITUDE"]) & (df["LATITUDE"] < bbox[2])]
+    # df = df[(bbox[0] < df["LONGITUD"]) & (df["LONGITUD"] < bbox[1]) & (bbox[3] < df["LATITUDE"]) & (
+    #         df["LATITUDE"] < bbox[2])]
 
-    with plt.style.context('seaborn-white'):
-        fig, ax = plt.subplots(dpi=600)
-        ax.scatter(df.LONGITUD, df.LATITUDE, zorder=1, alpha=0.8, s=10)
+    harm_column = 'Harm'
+    metadata_column = 'Metadata'
+    filter_column = 'Filter'
+    df.insert(2, harm_column, value='')
+    metadata = pd.DataFrame()
+    for index, row in df.iterrows():
+        response = query_crash_api(api='GetCaseDetails',
+                                   params={'stateCase': row['ST_CASE'], 'caseYear': row['CaseYear'], 'state': state_id})
+        crash_result_set = response['Results'][0][0]['CrashResultSet']
+        df.at[index, metadata_column] = response['Results'][0][0]
+        df.at[index, harm_column] = crash_result_set['HARM_EVNAME']
+        df.at[index, filter_column] = any([int(p['INJ_SEV']) >= 4 for p in get_people(crash_result_set)])
+        metadata.append(response['Results'][0][0]['CrashResultSet'], ignore_index=True)
 
-        ax.set_xlim(bbox[0], bbox[1])
-        ax.set_ylim(bbox[3], bbox[2])
-        image = plt.imread('map.png')
-        ax.imshow(image, zorder=0, extent=bbox, aspect='equal', interpolation='none', origin='lower')
+    df = df[df['Filter']]
 
-        plt.show()
+    with open('geojson.json', 'w') as outfile:
+        json.dump(convert_to_geojson(df), outfile)
 
 
 if __name__ == '__main__':
---
+++ script.js
@@ -0,0 +1,43 @@
+let map = L.map('map').setView([39.00, -76.88], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic3dpZ2RlciIsImEiOiJja29hbnI2bmQwMm0zMm91aHhlNHlhOHF2In0.FaLm4CYTTue7x4-NWm8p5g'
+}).addTo(map);
+
+const emojis = {
+    'car': '🚙',
+    'bike': '🚴',
+    'ped': '🚶',
+    'other': ''
+}
+
+$.getJSON('geojson.json', function (data) {
+    L.geoJSON(data, {
+        style: function (feature) {
+            return feature.properties && feature.properties.style;
+        },
+
+        onEachFeature: function (feature, layer) {
+            // layer.bindPopup(feature.properties.year);
+            layer.bindPopup(feature.properties.year);
+        },
+
+        pointToLayer: function (feature, latlng) {
+            const size = 20
+            let harm = feature.properties.harm
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    iconSize: [size, size],
+                    // iconAnchor: [size / 2, size + 9],
+                    className: `circle ${harm}`,
+                    html: `${emojis[harm]}️`,
+                })
+            });
+        }
+    }).addTo(map);
+});
