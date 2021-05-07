bcf8bb00332bd9a3cc0258f9c282b1fed965ecb1
suri <swigder@gmail.com>
2021-05-07 00:05:48 -0400

    Factor out generate_geojson to own file.

---
+++ generate_geojson.py
@@ -0,0 +1,40 @@
+import geojson
+
+
+def get_category(people):
+    per_typ_to_category = {v: k for k, values in {
+        'car': [1, 2, 3, 4],
+        'ped': [5, 8],
+        'bike': [6],
+        'other': [9, 10, 11],
+    }.items() for v in values}
+
+    max_category = 0
+    max_inj_sev = 0
+    for p in people:
+        inj_sev = int(p['INJ_SEV'])
+        category = int(p['PER_TYP'])
+        if (inj_sev > max_inj_sev) or (inj_sev == max_inj_sev and category > max_category):
+            max_category = category
+            max_inj_sev = inj_sev
+    return per_typ_to_category[max_category]
+
+
+def get_injuries(people):
+    injuries = []
+    for p in people:
+        injuries.append({'severity': p['INJ_SEVNAME'], 'person': p['PER_TYPNAME'].split()[0]})
+    return injuries
+
+
+def convert_to_geojson(df):
+    features = []
+    for index, row in df.iterrows():
+        features.append(geojson.Feature(geometry=geojson.Point((row['LONGITUD'], row['LATITUDE'])), properties={
+            'year': row['CaseYear'],
+            'case_id': row['ST_CASE'],
+            'harm': get_category(row['People']),
+            'injuries': get_injuries(row['People']),
+            'num_vehicles': len(row['Vehicles']),
+        }))
+    return geojson.FeatureCollection(features=features)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 
 import pandas as pd
 import requests
-import geojson
+
+from generate_geojson import convert_to_geojson
 
 BASE_URL = 'https://crashviewer.nhtsa.dot.gov'
 CRASH_API = f'{BASE_URL}/CrashAPI/crashes'
@@ -32,50 +33,11 @@ def query_crash_api(api, params, cache=True):
     return response
 
 
-def convert_to_geojson(df):
-    features = []
-    for index, row in df.iterrows():
-        features.append(geojson.Feature(geometry=geojson.Point((row['LONGITUD'], row['LATITUDE'])), properties={
-            'year': row['CaseYear'],
-            'case_id': row['ST_CASE'],
-            'harm': get_category(row['People']),
-            'injuries': list_injuries(row['People']),
-            'num_vehicles': len(row['Vehicles']),
-        }))
-    return geojson.FeatureCollection(features=features)
-
-
 def get_people(crash_result_set):
     people = crash_result_set['NPersons'] or []
     for vehicle in crash_result_set['Vehicles']:
         people.extend(vehicle['Persons'])
     return people
-
-
-def get_category(people):
-    per_typ_to_category = {v: k for k, values in {
-        'car': [1, 2, 3, 4],
-        'ped': [5, 8],
-        'bike': [6],
-        'other': [9, 10, 11],
-    }.items() for v in values}
-
-    max_category = 0
-    max_inj_sev = 0
-    for p in people:
-        inj_sev = int(p['INJ_SEV'])
-        category = int(p['PER_TYP'])
-        if (inj_sev > max_inj_sev) or (inj_sev == max_inj_sev and category > max_category):
-            max_category = category
-            max_inj_sev = inj_sev
-    return per_typ_to_category[max_category]
-
-
-def list_injuries(people):
-    injuries = []
-    for p in people:
-        injuries.append({'severity': p['INJ_SEVNAME'], 'person': p['PER_TYPNAME'].split()[0]})
-    return injuries
 
 
 def get_crashes_list(year_range=(2015, 2019), state_id=24, county_id=33):
