8f770aab767b2b7d04e744f0bd27f8b6c516982e
suri <swigder@gmail.com>
2021-05-24 21:53:54 -0400

    Fix how injuries & people are categorized. Simplify detail view.

--- generate_web_data.py
+++ generate_web_data.py
@@ -1,36 +1,85 @@
+from collections import namedtuple, defaultdict
+
 import geojson
+
+PersonType = namedtuple('PersonType', ['name', 'category'])
+InjuryType = namedtuple('InjuryType', ['name', 'category', 'number'])
+
+PER_TYPE = {
+    1: PersonType('Driver', 'car'),
+    2: PersonType('Passenger', 'car'),
+    3: PersonType('Occupant, motor vehicle not in-transport', 'car'),
+    4: PersonType('Occupant, non-motor vehicle transport device', 'car'),
+    5: PersonType('Pedestrian', 'ped'),
+    6: PersonType('Bicyclist', 'bike'),
+    7: PersonType('Cyclist (other than bicyclist)', 'bike'),
+    8: PersonType('Person on personal conveyances', 'ped'),
+    9: PersonType('Unknown (occupant of motor vehicle in-transport)', 'other'),
+    10: PersonType('Person in building', 'other'),
+    19: PersonType('Unknown (non-motorist)', 'other'),
+}
+UNKNOWN_PER_TYPE = PersonType('Unknown', 'other')
+PER_TYPE_PRIORITIES = [  # increasing order
+    'car', 'other', 'bike', 'ped',
+]
+INJURY_TYPE = {
+    0: InjuryType('No apparent injury', 'others', 0),
+    1: InjuryType('Possible injury', 'injuries', 1),
+    2: InjuryType('Suspected minor injury', 'injuries', 2),
+    3: InjuryType('Suspected serious injury', 'injuries', 3),
+    4: InjuryType('Fatal injury', 'fatalities', 4),
+    5: InjuryType('Injury, severity unknown', 'injuries', 5),
+    6: InjuryType('Died prior to crash', 'others', 6),
+}
+UNKNOWN_INJURY_TYPE = InjuryType('Unknown', 'others', -1)
+
+
+def invert_dict(orig, key):
+    inv = {}
+    for k, v in orig.items():
+        inv.setdefault(v._asdict()[key], []).append(k)
+    return inv
+
+
+INJURY_TYPE_GROUPS = invert_dict(INJURY_TYPE, 'category')
+
+
+def get_person_type(person):
+    return PER_TYPE.get(person['PER_TYP'], UNKNOWN_PER_TYPE)
+
+
+def get_injury_type(person):
+    return INJURY_TYPE.get(person['INJ_SEV'], UNKNOWN_INJURY_TYPE)
 
 
 def get_category(people):
-    per_typ_to_category = {v: k for k, values in {
-        'car': [1, 2, 3, 4],
-        'ped': [5, 8],
-        'bike': [6, 7],
-        'other': [9, 10, 19],
-    }.items() for v in values}
-
-    max_category = 0
-    max_inj_sev = 0
+    max_category = -1
     for p in people:
-        inj_sev = max(int(p['INJ_SEV']), 4)
-        category = int(p['PER_TYP'])
-        if (inj_sev > max_inj_sev) or (inj_sev == max_inj_sev and category > max_category):
+        if not get_injury_type(p).category == 'fatalities':
+            continue
+        category = PER_TYPE_PRIORITIES.index(get_person_type(p).category)
+        if category > max_category:
             max_category = category
-            max_inj_sev = inj_sev
-    return per_typ_to_category[max_category]
+    return PER_TYPE_PRIORITIES[max_category] if max_category > -1 else 'other'
 
 
 def get_injuries(people):
-    injuries = []
+    injuries = defaultdict(list)
     for p in people:
-        injuries.append({'severity': p['INJ_SEVNAME'], 'person': p['PER_TYPNAME'].split()[0]})
+        injury_type = get_injury_type(p)
+        person_type = get_person_type(p).name
+        if len(INJURY_TYPE_GROUPS[injury_type.category]) > 1:
+            info = {'severity': injury_type.name, 'person': person_type}
+        else:
+            info = person_type
+        injuries[injury_type.category].append(info)
     return injuries
 
 
 def get_num_fatalities(people):
     num_fatalities = 0
     for p in people:
-        if int(p['INJ_SEV']) >= 4:
+        if int(p['INJ_SEV']) == 4:
             num_fatalities += 1
     return num_fatalities
 
@@ -48,10 +97,11 @@ def convert_to_web_data(df):
             'num_fatalities': get_num_fatalities(row['Person']),
         }))
 
-        items_details[case_id] = {
+        details = {
             'year': row['CASEYEAR'],
             'num_vehicles': row['Vehicle'],
-            'injuries': get_injuries(row['Person'])
         }
+        details.update(get_injuries(row['Person']))
+        items_details[case_id] = details
 
     return geojson.FeatureCollection(features=geojson_items), items_details
